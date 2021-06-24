4597f1d49e8e6494e6f7ce98a6b0e29e045d79b0
chenxinyu6666 <81809614+chenxinyu6666@users.noreply.github.com>
2021-06-24 15:32:28 +0800

    Update BeerSelect.java

--- BeerE/src/main/java/com/example/BeerE/BeerSelect.java
+++ BeerE/src/main/java/com/example/BeerE/BeerSelect.java
@@ -1,13 +1,10 @@
 package com.example.BeerE;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Iterator;
 import java.util.List;
 
 @WebServlet("/SelectBeer.do")
